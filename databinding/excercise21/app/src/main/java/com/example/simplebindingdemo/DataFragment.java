7ff5614494080a246ebab402b27722b4fa168a49
llitaa <antonlytvynenko@gmail.com>
2018-06-18 12:37:46 +0200

    Added layout files

---
+++ databinding/excercise21/app/src/main/java/com/example/simplebindingdemo/DataFragment.java
@@ -0,0 +1,26 @@
+package com.example.simplebindingdemo;
+
+import android.app.Fragment;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.simplebindingdemo.databinding.FragmentDataBinding;
+
+public class DataFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+//        FragmentDataBinding binding = FragmentDataBinding.inflate(inflater, container, false);
+
+        inflater.inflate(R.layout.fragment_data, container, false);
+        FragmentDataBinding binding = FragmentDataBinding.inflate(inflater);
+
+        DataSource dataSource = DataSource.get("Fragment Binding");
+        binding.setDataSource(dataSource);
+        return binding.getRoot();
+    }
+}
