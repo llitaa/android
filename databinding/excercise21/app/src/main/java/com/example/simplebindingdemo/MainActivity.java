96a784d2f4d405a056bf9c416d18731ab500dd7f
llitaa <antonlytvynenko@gmail.com>
2018-06-26 16:24:02 +0200

    added recycler view

--- databinding/excercise21/app/src/main/java/com/example/simplebindingdemo/MainActivity.java
+++ databinding/excercise21/app/src/main/java/com/example/simplebindingdemo/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.simplebindingdemo;
 
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -14,6 +18,7 @@ import android.widget.TextView;
 // import com.example.simplebindingdemo.databinding.ActivityMainBinding;
 import com.example.simplebindingdemo.databinding.ActivityMainBinding;
 import com.example.simplebindingdemo.databinding.ExpressionsBinding;
+import com.example.simplebindingdemo.databinding.ItemViewBinding;
 // import com.example.simplebindingdemo.databinding.DataBindingUtils;
 
 //impoty android.databinding.DataBindingUtil;
@@ -33,6 +38,41 @@ public class MainActivity extends FragmentActivity {
         binding.setDataSource(dataSource);
         binding.setIncludeSource(includeSource);
 
-        ExpressionsBinding raspberryBinding = ExpressionsBinding.inflate(getLayoutInflater(), binding.menu, false);
+        // ExpressionsBinding raspberryBinding = ExpressionsBinding.inflate(getLayoutInflater(), binding.menu, false);
+        binding.list.setLayoutManager(new LinearLayoutManager(this));
+        binding.list.setAdapter(new DataSourceAdaper(getLayoutInflater()));
+    }
+
+    private class DataSourceAdaper extends RecyclerView.Adapter<ViewHolder>{
+        private LayoutInflater layoutInflater;
+
+        public DataSourceAdaper(LayoutInflater layoutInflater) {
+            this.layoutInflater = layoutInflater;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            ItemViewBinding binding = ItemViewBinding.inflate(layoutInflater, parent, false);
+            return new ViewHolder(binding.getRoot());
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            DataSource dataSource = DataSource.get("Item Binding" + position);
+            ItemViewBinding binding = DataBindingUtil.getBinding(holder.itemView);
+            binding.setDataSource(dataSource);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
