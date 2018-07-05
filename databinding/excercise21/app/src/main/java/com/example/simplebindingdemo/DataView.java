012c3ee089d7199917c1b980c514a6d703a86bf4
llitaa <antonlytvynenko@gmail.com>
2018-07-05 16:34:40 +0200

    Added text styles

--- databinding/excercise21/app/src/main/java/com/example/simplebindingdemo/DataView.java
+++ databinding/excercise21/app/src/main/java/com/example/simplebindingdemo/DataView.java
@@ -1,10 +1,14 @@
 package com.example.simplebindingdemo;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.databinding.DataBindingUtil;
+import android.widget.TextView;
 
 import com.example.simplebindingdemo.databinding.DataViewBinding;
 
@@ -27,5 +31,11 @@ public class DataView extends LinearLayout {
 //        binding = DataViewBinding.bind(this);
         DataSource dataSource = DataSource.get("Data View");
         binding.setDataSource(dataSource);
+        // Apply gradient
+        TextView tw = (TextView) binding.textViewWithGradient;
+        LinearGradient ls = new LinearGradient(0,0, 0, tw.getTextSize(), Color.GREEN, Color.RED, Shader.TileMode.REPEAT);
+        tw.getPaint().setShader(ls);
+        tw.setTextSize(40);
+
     }
 }
